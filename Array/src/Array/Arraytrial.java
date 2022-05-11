adebc01a76dfd91a82454f6bf681733ae091dcbf
Nikhil-kk <nikhilkatkar27@gmail.com>
2022-05-11 19:43:01 +0530

    trial

---
+++ Array/src/Array/Arraytrial.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Arraytrial {
+	public static void main(String[] args) {
+	double [] bose = new double [] {1,2,3,4,5};
+	for (double num2:bose) {
+		System.out.println(num2);
+	}
+	}
+
+}
